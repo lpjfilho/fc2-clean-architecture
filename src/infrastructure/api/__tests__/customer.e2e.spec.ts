82b53af30bd42fe3419907754cbd9dd34183aaf0
Luiz Pedro <luiz.filho@arizona.global>
2022-11-28 09:52:34 -0300

    feat: adiciona e2e test

---
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,93 @@
+import {app, sequelize} from "../express";
+import request from 'supertest'
+
+describe('E2E test for customer', () => {
+
+    beforeEach(async () => {
+        await sequelize.sync({ force: true })
+    })
+
+    afterAll(async () => {
+        await sequelize.close()
+    })
+
+    it('should create a customer', async () => {
+        const response = await request(app)
+            .post('/customer')
+            .send({
+                name: 'John',
+                address: {
+                    street: 'Street',
+                    number: 1,
+                    zip: '123',
+                    city: 'SP',
+                    country: 'BR',
+                }
+            })
+
+        expect(response.status).toBe(200)
+        expect(response.body.name).toBe('John')
+        expect(response.body.address.street).toBe('Street')
+        expect(response.body.address.number).toBe(1)
+        expect(response.body.address.zip).toBe('123')
+        expect(response.body.address.city).toBe('SP')
+        expect(response.body.address.country).toBe('BR')
+    })
+
+    it('should not create a customer', async () => {
+        const response = await request(app)
+            .post('/customer')
+            .send({
+                name: 'John',
+            })
+
+        expect(response.status).toBe(500)
+    })
+
+    it('should list all customer', async () => {
+        const response = await request(app)
+            .post('/customer')
+            .send({
+                name: 'John',
+                address: {
+                    street: 'Street',
+                    number: 1,
+                    zip: '123',
+                    city: 'SP',
+                    country: 'BR',
+                }
+            })
+
+        expect(response.status).toBe(200)
+
+        const response2 = await request(app)
+            .post('/customer')
+            .send({
+                name: 'Jane',
+                address: {
+                    street: 'Street',
+                    number: 1,
+                    zip: '1234',
+                    city: 'SP',
+                    country: 'BR',
+                }
+            })
+
+        expect(response2.status).toBe(200)
+
+        const response3 = await request(app)
+            .get('/customer')
+            .send()
+
+        expect(response3.status).toBe(200)
+        expect(response3.body.customers.length).toBe(2)
+
+        const customer = response3.body.customers[0]
+        expect(customer.name).toEqual('John')
+        expect(customer.address.zip).toEqual('123')
+
+        const customer2 = response3.body.customers[1]
+        expect(customer2.name).toEqual('Jane')
+        expect(customer2.address.zip).toEqual('1234')
+    })
+})
---
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -0,0 +1,48 @@
+import {app, sequelize} from "../express";
+import request from 'supertest'
+
+describe('E2E test for produt', () => {
+
+    beforeEach(async () => {
+        await sequelize.sync({force: true})
+    })
+
+    afterAll(async () => {
+        await sequelize.close()
+    })
+
+    it('should create a product', async () => {
+        const response = await request(app)
+            .post('/product')
+            .send({
+                name: 'Product',
+                price: 10,
+            })
+
+        console.log(response)
+
+        expect(response.status).toBe(200)
+
+        expect(response.body.name).toBe('Product')
+        expect(response.body.price).toBe(10)
+    })
+
+    it('should list all products', async () => {
+        const response = await request(app)
+            .post('/product')
+            .send({
+                name: 'Product',
+                price: 10,
+            })
+
+        expect(response.status).toBe(200)
+
+        const response2 = await request(app)
+            .get('/product')
+            .send()
+
+        const product = response2.body.products[0]
+        expect(product.name).toEqual('Product')
+        expect(product.price).toEqual(10)
+    })
+})
